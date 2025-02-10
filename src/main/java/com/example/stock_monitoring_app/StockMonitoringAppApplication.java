1d7befb552bc7980f29af8023cf3c5fbcf517766
Yalemsew Mekuriaw <yalemone@gmail.com>
2025-02-09 20:27:10 -0800

    project intilization

---
+++ src/main/java/com/example/stock_monitoring_app/StockMonitoringAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.stock_monitoring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMonitoringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockMonitoringAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stock_monitoring_app/StockMonitoringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stock_monitoring_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMonitoringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
